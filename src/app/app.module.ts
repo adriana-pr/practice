0ff9bb61f2bbcddd120366d615174656ed586f7b
adriana <tt2607224@gmail.com>
2023-04-29 19:26:28 +0300

    practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ElectronicsComponent } from './electronics/electronics.component';
+import { JeweleryComponent } from './jewelery/jewelery.component';
+import { MenClothingComponent } from './men-clothing/men-clothing.component';
+import { WomenClothingComponent } from './women-clothing/women-clothing.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServiceService } from './service/service.service';
+import { BasketComponent } from './basket/basket.component';
+import { ProductComponent } from './product/product.component';
+
+export function loadConfig(service : ServiceService) {
+  return()=>{
+  service.setProducts();
+  }
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ElectronicsComponent,
+    JeweleryComponent,
+    MenClothingComponent,
+    WomenClothingComponent,
+    FooterComponent,
+    BasketComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: loadConfig,
+      deps: [ServiceService],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
